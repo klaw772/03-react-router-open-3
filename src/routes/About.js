8ec01f770b6429c54ac5fa89e49944598581047e
Kat Law <kathleen.law@multiverse.io>
2023-02-15 16:21:11 -0500

    init

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>This is a React Router app!</h1>
+      <h2>🚀 nice!</h2>
+    </div>
+  );
+}
+
+export { About };
